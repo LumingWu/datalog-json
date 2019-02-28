9e170c0b05e451dd3b10b2db635da2c234dc4d6f
Luming Wu <reallumingwu@gmail.com>
2019-02-28 16:18:32 -0500

    Updates

---
+++ precache-manifest.2fd9fd72694f0b483a852523b8058c0e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b33357c673e08d996c5",
+    "url": "/datalog-json/static/js/runtime~main.4b33357c.js"
+  },
+  {
+    "revision": "de5dcf2edb06e0c1c87d",
+    "url": "/datalog-json/static/js/main.de5dcf2e.chunk.js"
+  },
+  {
+    "revision": "122a8b5b5918fc90e9da",
+    "url": "/datalog-json/static/js/2.122a8b5b.chunk.js"
+  },
+  {
+    "revision": "de5dcf2edb06e0c1c87d",
+    "url": "/datalog-json/static/css/main.fe33d93e.chunk.css"
+  },
+  {
+    "revision": "38b10096bc5ae285f96c9f238ffee800",
+    "url": "/datalog-json/index.html"
+  }
+];
